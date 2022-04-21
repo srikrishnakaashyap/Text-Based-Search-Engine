0b3af28f7626262de4871d1622d2230508dd4e62
Kaashyap <mskrishna.kaashyap@gmail.com>
2022-04-20 21:06:03 -0400

    Initial Setup

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_cors import CORS
+
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+# from initializers.register_all_blueprints import RegisterBlueprints
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_port=1)
+
+with app.app_context():
+    # from services.setup_config import SetupConfig
+    # SetupConfig(app)
+    # db.init_app(app)
+
+    # RegisterBlueprints(app, db)
+    CORS(app)
+    app.config['JWT_TOKEN_LOCATION'] = ['headers']
+    app.run(host = "0.0.0.0", port=5002,)
+
+    print("...Default Web Mode Completed")
